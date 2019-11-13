48aef28d8fb0e23f3945934ef570cb2dc738afe6
yaelBrown <yaelrbrown@gmail.com>
2019-11-13 11:53:58 -0600

    Able to get home/index.html page to load for home page

--- djangoBlog/djangoBlog/urls.py
+++ djangoBlog/djangoBlog/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('', include('home.urls')),
-    url('/', include('home.urls')),
-    url('posts/', include('posts.url')),
-    url('users/', include('users.url'))
+    url('/', include('home.urls'))
+    # url('posts/', include('posts.url')),
+    # url('users/', include('users.url'))
 ]
--- djangoBlog/home/urls.py
+++ djangoBlog/home/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
-  url('/', views.home, name='index')
-  url('home/', views.home, name='index')
+  url('', views.home, name='home'),
+  # url('home/', views.home, name='index')
 ]
