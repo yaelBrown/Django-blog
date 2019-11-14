d11b6d361dc1274c1c6e612af89c6af6cd94e35f
Yael Brown <yaelrbrown@gmail.com>
2019-11-13 21:52:07 -0600

    Getting sleepy, end of night push.

--- djangoBlog/home/models.py
+++ djangoBlog/home/models.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# Django field types - https://docs.djangoproject.com/en/2.2/ref/models/fields/#field-types
+
+class Post(models.Model):
+  title = models.CharField(max_length=100)
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  content = models.TextField()
