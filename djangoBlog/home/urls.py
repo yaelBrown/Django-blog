b5e165f850f8854e203575d45240e492454de6aa
Yael Brown <yaelrbrown@gmail.com>
2019-11-13 21:27:06 -0600

    Added admin page back

--- djangoBlog/home/urls.py
+++ djangoBlog/home/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
+  url('admin', admin.site.urls),
   url('login', views.userLogin, name='login'),
   url('register', views.userRegister, name='register'),
   url('create', views.postCreate, name='createPost'),
